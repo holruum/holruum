d10e9db55d1fef85cd4f4e525bcf5fef20409add
VenoM <sebastianbrunner@hotmail.ch>
2018-09-20 15:40:27 +0200

    start adding players. Added vue-bootstrap

---
+++ src/components/Players.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="Players">
+        <ul id="players">
+            <li v-for="player in Players">
+                {{ player.name }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Players',
+        data: () => {
+            return {
+            Players: [
+                {name: "Kurt"}
+            ]
+            }
+        }
+    }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,29 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <b-input-group v-for="size in ['']" :key="size" :size="size" class="mb-3" prepend="Playername">
+        <b-form-input />
+        <b-input-group-append>
+          <b-btn size="sm" text="Button" variant="success">Add Player</b-btn>
+        </b-input-group-append>
+      </b-input-group>
+    </div>
+    <h1>Players:</h1>
+    <Players Players="Kurt" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Players from '@/components/Players.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,
+      Players
   }
 }
 </script>
