278d4d15ffb1cb2e603d0f58322284e3c1a25dc6
VenoM <sebastianbrunner@hotmail.ch>
2018-09-20 17:34:05 +0200

    Added svg functionality. Improved player-form

--- src/components/Players.vue
+++ src/components/Players.vue
@@ -1,10 +1,25 @@
 <template>
     <div class="Players">
-        <ul id="players">
-            <li v-for="player in Players">
-                {{ player.name }}
-            </li>
-        </ul>
+        <div>
+            <b-input-group class="mb-3" prepend="Add a Player:" >
+                <b-form-input @keyup.enter.native="addPlayer" v-model="playerName" />
+                <b-input-group-append>
+                    <b-btn size="sm" text="Button" variant="success" @click="addPlayer">Add Player</b-btn>
+                </b-input-group-append>
+            </b-input-group>
+        </div>
+        <h4>List of Players</h4>
+        <div class="list-group-item" v-if="Players.length == 0">
+            No players registered
+        </div>
+        <div class="list-group-item" v-if="Players.length > 2">
+            Too many players registered. Only 2 players are possible.
+        </div>
+        <div class="list-group-item" v-for="player in Players">
+            {{ player.name }} <button class="btn btn-xs btn-danger" @click="deleteEvent(player)">Delete</button>
+        </div>
+        <b-button disabled v-if="Players.length == 0 || Players.length > 2" variant="success" href="/play">Start game!</b-button>
+        <b-button v-if="Players.length != 0 && Players.length == 2" variant="success" href="/play">Start game!</b-button>
     </div>
 </template>
 
@@ -13,9 +28,19 @@
         name: 'Players',
         data: () => {
             return {
+                fields: ['Playername', ' '],
             Players: [
-                {name: "Kurt"}
-            ]
+
+            ],
+                playerName: ""
+            }
+        },
+        methods: {
+            addPlayer: function(){
+                this.Players.push({name: this.playerName});
+            },
+            deleteEvent: function(player) {
+                this.Players.splice(this.Players.indexOf(player), 1);
             }
         }
     }
