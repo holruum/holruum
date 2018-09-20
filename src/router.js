cd798ecb57dfaf118248e826d5a8d49ded3e2775
VenoM <sebastianbrunner@hotmail.ch>
2018-09-20 17:40:42 +0200

    Added default guide

--- src/router.js
+++ src/router.js
@@ -18,7 +18,12 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    },
+      {
+          path: '/guide',
+          name: 'guide',
+          component: () => import(/* webpackChunkName: "about" */ './views/Guide.vue')
+      }
   ],
   mode: 'history'
 })
---
+++ src/views/Guide.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="guide">
+        <h1>Guide</h1>
+        An awesome guide will be here in the future!
+    </div>
+</template>
