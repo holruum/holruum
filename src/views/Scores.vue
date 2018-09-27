f12de3fda0e35323732d63349274da7c5f50f553
VenoM <sebastianbrunner@hotmail.ch>
2018-09-27 11:26:54 +0200

    starting on score site

--- src/views/Scores.vue
+++ src/views/Scores.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="scores">
     <h1>Score</h1>
+    <b-progress class="mt-1" :max="$store.state.scores[0] + $store.state.scores[1]" show-value>
+      <b-progress-bar :value="$store.state.scores[0]" variant="warning"></b-progress-bar>
+      <b-progress-bar :value="$store.state.scores[1]" variant=#4575b4></b-progress-bar>
+    </b-progress>
     {{$store.state.players[$store.state.scores[0] > $store.state.scores[1] ? 0 : 1]}}
     Player 1 score: {{$store.state.scores[0]}}
     Player 2 score: {{$store.state.scores[1]}}
