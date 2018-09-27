7fb1d0fad9bfa381c57759c32c4beadb856de74f
VenoM <sebastianbrunner@hotmail.ch>
2018-09-27 08:37:18 +0200

    store-things

--- src/components/Players.vue
+++ src/components/Players.vue
@@ -2,32 +2,31 @@
     <div class="Players">
         <div>
             <b-input-group class="mb-3" prepend="Add a Player:" >
-                <b-form-input @keyup.enter.native="addPlayer" v-model="playerName" />
+                <b-form-input @keyup.enter.native="addPlayer" v-model="this.$store.state.playerName" />
                 <b-input-group-append>
                     <b-btn size="sm" text="Button" variant="success" @click="addPlayer">Add Player</b-btn>
                 </b-input-group-append>
             </b-input-group>
         </div>
         <h4>List of Players</h4>
-        <div class="list-group-item" v-if="Players.length == 0">
+        <div class="list-group-item" v-if="this.$store.state.Players.length == 0">
             No players registered
         </div>
-        <div class="list-group-item" v-if="Players.length > 2">
+        <div class="list-group-item" v-if="this.$store.state.Players.length > 2">
             Too many players registered. Only 2 players are possible.
         </div>
-        <div v-bind:key="id" class="list-group-item" v-for="(player,id) in Players">
+        <div v-bind:key="id" class="list-group-item" v-for="(player,id) in this.$store.state.Players">
             {{ player.name }} <button class="btn btn-xs btn-danger" @click="deleteEvent(player)">Delete</button>
         </div>
-        <b-button disabled v-if="Players.length == 0 || Players.length > 2" variant="success" to="/play">Start game!</b-button>
-        <b-button v-if="Players.length != 0 && Players.length == 2" variant="success" to="/play">Start game!</b-button>
+        <b-button disabled v-if="this.$store.state.Players.length == 0 || this.$store.Players.length > 2" variant="success" to="/play">Start game!</b-button>
+        <b-button v-if="this.$store.state.Players.length != 0 && this.$store.state.Players.length == 2" variant="success" to="/play">Start game!</b-button>
     </div>
 </template>
 
 <script>
-    import Vue from 'vue'
     import Vuex from 'vuex'
+    import Vue from 'vue'
     import 'es6-promise/auto'
-
     Vue.use(Vuex);
 
     const store = new Vuex.Store({
