0bd8f020a0d5935fe04555b7fbc479229b028e4a
VenoM <sebastianbrunner@hotmail.ch>
2018-09-29 19:06:18 +0200

    added some text to the about page...

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Hey, we are:</h1>
+    <link>Just a few students from the ZHAW which need to do a FREAKING AWESOME PROJECT for the course MOBA.<br>
+    So we started to think about something funny. A game who everyone can play with just 1 minute learning.<br>
+    But you'll find more to "how to play this awesome game" at the <b-link href="/guide">GUIDE</b-link> <br>
+    <br>
+    Yes, that's all. Have fun! <br>
   </div>
 </template>
