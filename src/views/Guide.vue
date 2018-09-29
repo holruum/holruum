5c6e5be9ce0a06dc8f62b60170da64db94ca9e7e
VenoM <sebastianbrunner@hotmail.ch>
2018-09-29 19:12:23 +0200

    starting the guide out of a funny perspective which doesnt like everyone...

--- src/views/Guide.vue
+++ src/views/Guide.vue
@@ -1,6 +1,19 @@
 <template>
     <div class="guide">
         <h1>Guide</h1>
-        An awesome guide will be here in the future!
+        Yes, this is the guide... <br>
+        If you get onto our homepage you need to enter a few playernames. The game works only with two people. <br>
+        You can of course enter more or less people than these. But then you won't be able to start the game. Your choice :) <br>
+        If you are finally able to enter EXACTLY TWO playernames. Then the <b-button variant="success" to="">Start game!</b-button> Button appears. <br>
+        Click on it. <br>
+        ... <br>
+        ... <br>
+        ... <br>
+        CLICK NOW ON IT!!! <br>
+        Yes that one doesn't work. But the one at the start-page works. <br>
+        The game starts now. The first player can start and set a first line. <br>
+        After that, you can assume what happens now: The second player can set a line <br>
+        Awesome what? <br>
+        So the whole game is based on setting lines and create squares. If you get a square you can 
     </div>
 </template>
