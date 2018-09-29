0edd5fffb4b50b0fe90f72a2fae3b23e3c450ac6
VenoM <sebastianbrunner@hotmail.ch>
2018-09-29 18:55:38 +0200

    finally the correct colors in the progress-bar. Yeeehaw! But Winner could be improved...

--- src/views/Scores.vue
+++ src/views/Scores.vue
@@ -4,18 +4,17 @@
     <h3>Winner: {{$store.state.players[$store.state.scores[0] > $store.state.scores[1] ? 0 : 1]}}</h3>
     <h6>Score of {{$store.state.players[0]}}: {{$store.state.scores[0]}}</h6>
     <h6>Score of {{$store.state.players[1]}}: {{$store.state.scores[1]}}</h6>
-    <b-progress class="mt-1" :max="$store.state.scores[0] + $store.state.scores[1]" show-value>
+    <b-progress height="4rem" class="mt-1" :max="$store.state.scores[0] + $store.state.scores[1]" show-value>
       <b-progress-bar :value="$store.state.scores[0]" variant="warning">{{$store.state.players[0]}}</b-progress-bar>
       <b-progress-bar :value="$store.state.scores[1]" variant="primary">{{$store.state.players[1]}}</b-progress-bar>
     </b-progress>
   </div>
 </template>
-<style lang="css" scoped>
-  .b-progress-bar.bg-warning {
+<style scoped lang="scss">
+  .bg-warning {
     background-color: #d73027 !important;
   }
-  .b-progress-bar.bg-primary {
+  .bg-primary {
     background-color: #4575b4 !important;
   }
-
 </style>
